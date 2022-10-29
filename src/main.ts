f3877f186962d12865ab31c705fdc343fe9c3bc1
solarkraft <>
2022-10-29 13:24:53 +0200

    Enable downloading epub file from first URL

--- src/main.ts
+++ src/main.ts
@@ -1,116 +1,51 @@
 import * as fs from "fs"
+import path from "path"
 
 import Koa, { Context } from "koa"
-import koaBody from "koa-body"
 import mount from "koa-mount"
 import serve from "koa-static"
 
 import { makeEpub } from "./epub.js" // https://github.com/TypeStrong/ts-node/issues/1777
 
-// Token, destination directory
-let directories = new Map<string, string>([
-	["a", "/tmp/"],
-	["b", "/data/reading/Web"],
-])
-
 const app = new Koa()
 
-app.use(mount("/", serve("./static")))
+app.use(async (ctx: Context, next) => {
+	// Ignore favicon requests
+	if (ctx.path.includes("favicon")) return
+	console.log("Processing", ctx.method,ctx.request.href)
 
-app.use(koaBody({ includeUnparsed: true }))
+	let query = ctx.request.query
 
-app.use(async (ctx: Context) => {
-	const path = ctx.path
-	console.log(ctx.method, path)
-
-	// We're DIYing the router because the built-in one makes it hard to get content behind a /
-	if (path.startsWith("/epub")) {
-		let url: URL
-		try {
-			let urlString = path.replace("/epub/", "")
-
-			// Nothing provided and GET
-			if (!urlString) {
-				return
-			}
-
-			url = new URL(urlString) // Error kinda expeced
-
-			if (url.protocol != "https:" && url.protocol != "http:") {
-				throw new Error(
-					"Sorry friend, this protocol isn't supported: " +
-						url.protocol
-				)
-			}
-		} catch (error) {
-			console.error(error)
-			ctx.status = 400
-			ctx.body = error
-			return
-		}
-
-		let epubPath = await makeEpub(url)
-		let epubName = epubPath.split("/").pop()
-
-		let epub = await fs.promises.readFile(epubPath)
-
-		ctx.set("content-type", "application/epub+zip")
-		ctx.set("content-disposition", `attachment; filename="${epubName}"`)
-
-		ctx.body = epub
-	} else if (path.startsWith("/save")) {
-		let token: string
-
-		try {
-			// /save/<token>/<url to save>
-			token = path.replace(/\/save\//, "").split("/")[0] // Error kinda expeced
-
-			ctx.body = token
-
-			let directory = directories.get(token)
-			if (!directory) {
-				throw new Error("Invalid token " + token)
-			}
-
-			// Authenticated
-			console.info(
-				"Authenticated.",
-				"Token:",
-				token,
-				"Directory:",
-				directory
-			)
-
-			// Get URL // Remove token
-			let urlString = path
-				.replace(/\/save\//, "")
-				.replace(token + "/", "")
-
-			// Welcome page if no URL given
-			if (!urlString) {
-				ctx.body = `Welcome. Append this URL with an article's to save it to your associated directory ${directory}`
-			}
-
-			let url = new URL(urlString)
-
-			// Make and save epub
-			let epubPath = await makeEpub(url)
-			let epubName = epubPath.split("/").pop()
-
-			let newPath = directory + "/" + epubName
-
-			console.info("Copying" + epubPath + " to " + newPath)
-			await fs.promises.copyFile(epubPath, newPath)
-
-			// Success message
-			ctx.body = `Saved article ${urlString} to ${newPath}`
-		} catch (error) {
-			console.error(error)
-			ctx.status = 400
-			ctx.body = error
-			return
-		}
+	// If no URL was provided, fall through to static serving
+	if (query.url === undefined) {
+		console.log("No URL, falling through")
+		await next()
+		return
 	}
+
+	// If a query parameter only comes in once, Koa provides it as a string. But we always want string[] for ease of processing.
+	let urls = Array.isArray(query.url) ? query.url : [query.url]
+	let titles = Array.isArray(query.title) ? query.title : [query.title]
+
+	// Queries come like { url: [ 'url1', 'url2' ], title: [ 'title1', 'title2' ] }
+	// Re-pack them to article objects [{url: "url1", title: "title1"}, {url: "url2", title: "title2"}]
+	let articles: { url: string, title?: string }[] = []
+	for (let i = 0; i < urls.length; i++) {
+		articles[i] = { url: urls[i], title: titles[i] }
+	}
+
+	console.log("Articles:", articles)
+
+	let filePath = await makeEpub(new URL(articles[0].url))
+
+	// Get file
+	let file = fs.createReadStream(filePath)
+
+	ctx.response.set("content-type", "application/epub+zip")
+	ctx.response.set("content-disposition", `attachment; filename=${path.basename(filePath)}`)
+	ctx.body = file
 })
+
+app.use(mount("/", serve("./static")))
 
 app.listen(3000)
