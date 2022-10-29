de12b1af5e96dcf6b1e86dc386ab77569d418a8a
solarkraft <>
2022-10-29 12:12:50 +0200

    Formatting

--- src/epub.ts
+++ src/epub.ts
@@ -1,18 +1,18 @@
-import * as fs from "fs";
-import { tmpdir } from "os";
-import path from "path/posix";
+import * as fs from "fs"
+import { tmpdir } from "os"
+import path from "path/posix"
 
-import Mercury from "@postlight/mercury-parser";
-import canvas from "canvas";
-import * as nodepub from "nodepub";
-import sanitizeHtml from "sanitize-html";
-import { decode } from "html-entities";
-import * as cheerio from "cheerio";
-import fetch from "node-fetch";
+import Mercury from "@postlight/mercury-parser"
+import canvas from "canvas"
+import * as nodepub from "nodepub"
+import sanitizeHtml from "sanitize-html"
+import { decode } from "html-entities"
+import * as cheerio from "cheerio"
+import fetch from "node-fetch"
 
 async function parsePage(url: URL) {
 	// Parse page using Mercury
-	let page = await Mercury.parse(url.toString());
+	let page = await Mercury.parse(url.toString())
 
 	// Sanitize HTML. Removes forbidden tags and cleans up some bad syntax like improperly closed tags.
 	let content = sanitizeHtml(page.content as string, {
@@ -67,44 +67,44 @@ async function parsePage(url: URL) {
 			"ul",
 			"var",
 		],
-	});
+	})
 
 	// Decode HTML entities
-	let description = decode(page.excerpt);
+	let description = decode(page.excerpt)
 
 	return { content, description, page }
 }
 
 export async function makeEpub(url: URL) {
-	let id = Math.random().toString().substr(2, 8);
+	let id = Math.random().toString().substr(2, 8)
 
-	let tmpDir = tmpdir() + `/epub/${id}/`;
+	let tmpDir = tmpdir() + `/epub/${id}/`
 	// console.log("tmpDir:", tmpDir); // Not /tmp, quite cryptic and random on macOS
 
-	await fs.promises.mkdir(tmpDir, { recursive: true });
+	await fs.promises.mkdir(tmpDir, { recursive: true })
 
-	console.info("Parsing web page " + url.toString());
+	console.info("Parsing web page " + url.toString())
 
 	let { content, description, page } = await parsePage(url)
-	
+
 	let createCover = async () => {
 		// Generate cover image
-		console.info("Generating cover image");
-		await createCoverImage(tmpDir + "cover.png", page.title || page.url);
-		console.info("Wrote cover image");
-	};
+		console.info("Generating cover image")
+		await createCoverImage(tmpDir + "cover.png", page.title || page.url)
+		console.info("Wrote cover image")
+	}
 
 	// List of image paths
-	let images = Array<string>();
+	let images = Array<string>()
 
 	let getImages = async () => {
-		console.info("Getting images");
-		[content, images] = await fetchAndReplaceImages(content, tmpDir);
-		console.info("Got all images");
-	};
+		console.info("Getting images")
+		;[content, images] = await fetchAndReplaceImages(content, tmpDir)
+		console.info("Got all images")
+	}
 
 	// Do these simultaneously because both can take a while
-	await Promise.all([createCover(), getImages()]);
+	await Promise.all([createCover(), getImages()])
 
 	// Metadata for the epub file
 	let meta = {
@@ -125,91 +125,91 @@ export async function makeEpub(url: URL) {
 
 		series: page.domain,
 		publisher: page.domain,
-	};
+	}
 
-	let book = nodepub.document(meta);
+	let book = nodepub.document(meta)
 
-	console.info("Creating E-Book");
+	console.info("Creating E-Book")
 
-	book.addSection(meta.title, content, false);
+	book.addSection(meta.title, content, false)
 
 	// Turn into (somewhat) friendly file name
-	let pageName = url.pathname.toString() + url.searchParams.toString();
+	let pageName = url.pathname.toString() + url.searchParams.toString()
 	let fileName = pageName
 		.replace(/[^a-z0-9\.]/gi, "_") // turn non-letters into underscores
 		.toLowerCase()
-		.replace(/^_+|_+$/g, ""); // strip underscores from beginning and end
+		.replace(/^_+|_+$/g, "") // strip underscores from beginning and end
 
-	console.info("Writing E-Book to " + fileName);
-	await book.writeEPUB("./", fileName);
+	console.info("Writing E-Book to " + fileName)
+	await book.writeEPUB("./", fileName)
 
-	console.info(`Writing file to ${tmpDir}/${fileName}.epub`);
-	await book.writeEPUB(tmpDir, fileName);
+	console.info(`Writing file to ${tmpDir}/${fileName}.epub`)
+	await book.writeEPUB(tmpDir, fileName)
 
-	let filePath = tmpDir + fileName + ".epub";
-	return filePath;
-};
+	let filePath = tmpDir + fileName + ".epub"
+	return filePath
+}
 
 async function createCoverImage(path: string, title: string) {
 	return new Promise<void>((resolve) => {
-		const coverImage = canvas.createCanvas(600, 800);
-		const ctx = coverImage.getContext("2d");
+		const coverImage = canvas.createCanvas(600, 800)
+		const ctx = coverImage.getContext("2d")
 
 		let drawTitle = (title: string, fontSize: number) => {
 			// Clear
-			ctx.fillStyle = "white";
-			ctx.fillRect(0, 0, coverImage.width, coverImage.height);
-			ctx.fillStyle = "black";
+			ctx.fillStyle = "white"
+			ctx.fillRect(0, 0, coverImage.width, coverImage.height)
+			ctx.fillStyle = "black"
 
-			ctx.font = fontSize + "px sans-serif";
+			ctx.font = fontSize + "px sans-serif"
 
-			let sideMargin = 50;
-			let topMargin = 200;
-			let bottomMargin = 100;
+			let sideMargin = 50
+			let topMargin = 200
+			let bottomMargin = 100
 
-			let x = sideMargin;
-			let y = topMargin; // Starting height
+			let x = sideMargin
+			let y = topMargin // Starting height
 
-			let lastLine = "";
-			let newLine = "";
+			let lastLine = ""
+			let newLine = ""
 
 			title.split(" ").forEach(async (word, i) => {
-				newLine = lastLine + word + " ";
+				newLine = lastLine + word + " "
 
 				if (ctx.measureText(newLine).width + x * 2 < coverImage.width) {
-					lastLine = newLine;
+					lastLine = newLine
 				} else {
 					// break line and write last one
-					ctx.fillText(lastLine, x, y);
+					ctx.fillText(lastLine, x, y)
 
 					// Continue on next line
-					y += fontSize;
+					y += fontSize
 
 					// Clear next line (except last word)
-					lastLine = word + " ";
+					lastLine = word + " "
 				}
-			});
+			})
 			// Write last line
-			ctx.fillText(lastLine, x, y);
+			ctx.fillText(lastLine, x, y)
 
 			// Did we exceed the canvas' height?
 			if (y + bottomMargin > coverImage.height) {
 				// Let's try again
-				drawTitle(title, fontSize - 20);
+				drawTitle(title, fontSize - 20)
 			}
-		};
+		}
 
-		drawTitle(title, 160);
+		drawTitle(title, 160)
 
 		// Save image
-		coverImage.createPNGStream();
+		coverImage.createPNGStream()
 
-		const out = fs.createWriteStream(path);
-		const stream = coverImage.createPNGStream();
-		out.on("finish", () => resolve());
+		const out = fs.createWriteStream(path)
+		const stream = coverImage.createPNGStream()
+		out.on("finish", () => resolve())
 
-		stream.pipe(out);
-	});
+		stream.pipe(out)
+	})
 }
 
 /** Download images from the web, place them in the temp folder and replace references in the HTML. returns the new content and the list of images */
@@ -218,71 +218,71 @@ async function fetchAndReplaceImages(
 	tmpPath: string
 ): Promise<[string, Array<string>]> {
 	return new Promise<[string, Array<string>]>(async (resolve, reject) => {
-		const html = cheerio.load(oldContent, { xml: true });
-		const root = html.root();
+		const html = cheerio.load(oldContent, { xml: true })
+		const root = html.root()
 
-		let imgElements = Array<cheerio.Element>();
-		html("img").map((_, img) => imgElements.push(img));
+		let imgElements = Array<cheerio.Element>()
+		html("img").map((_, img) => imgElements.push(img))
 
 		// Create list for files to be downloaded
 		// Original URL, new path, image element
-		let downloadList = Array<[string, string, cheerio.Element]>();
+		let downloadList = Array<[string, string, cheerio.Element]>()
 		imgElements.forEach((img) => {
-			let originalUrl = img.attribs["src"];
+			let originalUrl = img.attribs["src"]
 
 			// iBooks wants any image file extension to render.
 			// Doesn't *have to* match the actual file type, but a mismatch causes a warning.
-			let ext = path.parse(originalUrl).ext || "png";
-			let newName = Math.random().toString().substr(2, 8) + "." + ext;
+			let ext = path.parse(originalUrl).ext || "png"
+			let newName = Math.random().toString().substr(2, 8) + "." + ext
 
-			downloadList.push([originalUrl, newName, img]);
-		});
+			downloadList.push([originalUrl, newName, img])
+		})
 
-		let fetchPromises = Array<Promise<void>>();
+		let fetchPromises = Array<Promise<void>>()
 		downloadList.forEach(([originalUrl, newName, element]) => {
 			fetchPromises.push(
 				new Promise(async (resolve, reject) => {
 					// Get file
-					let response = await fetch(originalUrl);
+					let response = await fetch(originalUrl)
 
 					// Write file
 					await fs.promises.writeFile(
 						tmpPath + newName,
 						response.body || "failed"
-					);
+					)
 
 					// Replace reference ("../images/" is a hard coded path nodepub copies files to)
-					element.attribs["src"] = "../images/" + newName;
+					element.attribs["src"] = "../images/" + newName
 
 					// If there's no alt tag, add an empty one (avoids warning)
 					if (!element.attribs["alt"]) {
-						element.attribs["alt"] = "";
+						element.attribs["alt"] = ""
 					}
 
 					// Resolve promise
 					if (response.body) {
-						resolve();
+						resolve()
 					} else {
-						reject(new Error("Request failed"));
+						reject(new Error("Request failed"))
 					}
 				})
-			);
-		});
+			)
+		})
 
-		let images = Array<string>();
+		let images = Array<string>()
 		downloadList.forEach(([originalUrl, newName, element]) => {
-			images.push(tmpPath + newName);
-		});
+			images.push(tmpPath + newName)
+		})
 
 		// Download all images
-		await Promise.all(fetchPromises);
+		await Promise.all(fetchPromises)
 
 		// Write modified content
-		let newContent = root.html();
+		let newContent = root.html()
 		if (newContent) {
-			resolve([newContent, images]);
+			resolve([newContent, images])
 		} else {
-			reject(new Error("Re-rendering failed :("));
+			reject(new Error("Re-rendering failed :("))
 		}
-	});
+	})
 }
--- src/main.ts
+++ src/main.ts
@@ -1,70 +1,70 @@
-import * as fs from "fs";
+import * as fs from "fs"
 
-import Koa, { Context } from "koa";
-import koaBody from "koa-body";
+import Koa, { Context } from "koa"
+import koaBody from "koa-body"
 
-import { makeEpub } from "./epub.js"; // https://github.com/TypeStrong/ts-node/issues/1777
+import { makeEpub } from "./epub.js" // https://github.com/TypeStrong/ts-node/issues/1777
 
 // Token, destination directory
 let directories = new Map<string, string>([
 	["a", "/tmp/"],
 	["b", "/data/reading/Web"],
-]);
+])
 
-const app = new Koa();
-app.use(koaBody({ includeUnparsed: true }));
+const app = new Koa()
+app.use(koaBody({ includeUnparsed: true }))
 
 app.use(async (ctx: Context) => {
-	const path = ctx.path;
-	console.log(ctx.method, path);
+	const path = ctx.path
+	console.log(ctx.method, path)
 
 	// We're DIYing the router because the built-in one makes it hard to get content behind a /
 	if (path.startsWith("/epub")) {
-		let url: URL;
+		let url: URL
 		try {
-			let urlString = path.replace("/epub/", "");
+			let urlString = path.replace("/epub/", "")
 
 			// Nothing provided and GET
 			if (!urlString) {
-				return;
+				return
 			}
 
-			url = new URL(urlString); // Error kinda expeced
+			url = new URL(urlString) // Error kinda expeced
 
 			if (url.protocol != "https:" && url.protocol != "http:") {
 				throw new Error(
 					"Sorry friend, this protocol isn't supported: " +
 						url.protocol
-				);
+				)
 			}
 		} catch (error) {
-			console.error(error);
-			ctx.status = 400;
-			ctx.body = error;
-			return;
+			console.error(error)
+			ctx.status = 400
+			ctx.body = error
+			return
 		}
 
-		let epubPath = await makeEpub(url);
-		let epubName = epubPath.split("/").pop();
+		let epubPath = await makeEpub(url)
+		let epubName = epubPath.split("/").pop()
 
-		let epub = await fs.promises.readFile(epubPath);
+		let epub = await fs.promises.readFile(epubPath)
 
-		ctx.set("content-type", "application/epub+zip");
-		ctx.set("content-disposition", `attachment; filename="${epubName}"`);
+		ctx.set("content-type", "application/epub+zip")
+		ctx.set("content-disposition", `attachment; filename="${epubName}"`)
 
-		ctx.body = epub;
+		ctx.body = epub
 	} else if (path.startsWith("/save")) {
-		let token: string;
+		let token: string
 
 		try {
 			// /save/<token>/<url to save>
-			token = path.replace(/\/save\//, "").split("/")[0]; // Error kinda expeced
+			token = path.replace(/\/save\//, "").split("/")[0] // Error kinda expeced
 
-			ctx.body = token;
+			ctx.body = token
 
-			let directory = directories.get(token);
+			let directory = directories.get(token)
 			if (!directory) {
-				throw new Error("Invalid token " + token);
+				throw new Error("Invalid token " + token)
 			}
 
 			// Authenticated
@@ -74,38 +74,38 @@ app.use(async (ctx: Context) => {
 				token,
 				"Directory:",
 				directory
-			);
+			)
 
 			// Get URL // Remove token
 			let urlString = path
 				.replace(/\/save\//, "")
-				.replace(token + "/", "");
+				.replace(token + "/", "")
 
 			// Welcome page if no URL given
 			if (!urlString) {
-				ctx.body = `Welcome. Append this URL with an article's to save it to your associated directory ${directory}`;
+				ctx.body = `Welcome. Append this URL with an article's to save it to your associated directory ${directory}`
 			}
 
-			let url = new URL(urlString);
+			let url = new URL(urlString)
 
 			// Make and save epub
-			let epubPath = await makeEpub(url);
-			let epubName = epubPath.split("/").pop();
+			let epubPath = await makeEpub(url)
+			let epubName = epubPath.split("/").pop()
 
-			let newPath = directory + "/" + epubName;
+			let newPath = directory + "/" + epubName
 
-			console.info("Copying" + epubPath + " to " + newPath);
-			await fs.promises.copyFile(epubPath, newPath);
+			console.info("Copying" + epubPath + " to " + newPath)
+			await fs.promises.copyFile(epubPath, newPath)
 
 			// Success message
-			ctx.body = `Saved article ${urlString} to ${newPath}`;
+			ctx.body = `Saved article ${urlString} to ${newPath}`
 		} catch (error) {
-			console.error(error);
-			ctx.status = 400;
-			ctx.body = error;
-			return;
+			console.error(error)
+			ctx.status = 400
+			ctx.body = error
+			return
 		}
 	}
-});
+})
 
-app.listen(3000);
+app.listen(3000)
